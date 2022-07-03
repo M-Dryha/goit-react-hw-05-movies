c8242c6c5bacaa3960cf156d576768a17f1bfbe3
Mariia Dryha <m.pishchanska@gmail.com>
2022-07-03 13:41:06 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
-// import { Home } from 'components/Home';
-// import Movies from './Movies';
-// import MovieDetails from './MovieDetails';
-// import Reviews from './Reviews';
-// import Cast from './Cast';
+import { Home } from 'components/Home';
+import Movies from './Movies';
+import MovieDetails from './MovieDetails';
+import Reviews from './Reviews';
+import Cast from './Cast';
 import Navigate from './Navigate';
-// import NotFoundPage from './NotFound';
+import NotFoundPage from './NotFound';
 
-const Home = lazy(() =>
-  import('../components/Home').then(module => ({ default: module.Home }))
-);
+// const Home = lazy(() =>
+//   import('../components/Home').then(module => ({ default: module.Home }))
+// );
 
-const Movies = lazy(() => import('./Movies/Movies'));
-const Reviews = lazy(() => import('./Reviews/Reviews'));
-const MovieDetails = lazy(() => import('../components/MovieDetails'));
-const Cast = lazy(() => import('../components/Cast'));
+// const Movies = lazy(() => import('./Movies/Movies'));
+// const Reviews = lazy(() => import('./Reviews/Reviews'));
+// const MovieDetails = lazy(() => import('../components/MovieDetails'));
+// const Cast = lazy(() => import('../components/Cast'));
 // const Navigate = lazy(() => import('./Navigate/Navigate'));
-const NotFoundPage = lazy(() => import('./NotFound/NotFound'));
+// const NotFoundPage = lazy(() => import('./NotFound/NotFound'));
 
 export const App = () => {
   return (
@@ -31,7 +31,7 @@ export const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route path="/*" element={<NotFoundPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </>
